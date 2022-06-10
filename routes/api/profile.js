39ca659f3ce5cf840416158df6acc4a4317926e4
smhah <smhah@student.1337.ma>
2022-06-10 17:05:39 +0100

    s

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const router = express.Router();
-
-// @route	GET api/profile
-// @desc	test route
+const auth = require("../../middleware/auth");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+// @route	GET api/profile/me
+// @desc	Get current user profile
 // @access	public
 
-router.get("/", (req, res) => res.send("User profile"));
+router.get("/me", auth, async (req, res) => {
+    try{
+        const profile = await Profile.findOne({ user: req.user.id}).populate("user",
+    ["name","avatar"]);
+    if(!profile){
+        return res.status(400).json({msg: "There is no profile for this user"});
+    }
+    res.json(profile);
+    }
+    catch (err){
+        console.error(err.message);
+        req.status(500).send("Server error");
+    }
+});
 
 module.exports = router;
