95eb8bd4786afe54f90cb7741f0468eb1f34bf5f
smhah <smhah@student.1337.ma>
2022-06-10 14:56:13 +0100

    s

---
+++ routes/api/posts.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+// @route	GET api/posts
+// @desc	test route
+// @access	public
+
+router.get("/", (req, res) => res.send("User posts"));
+
+module.exports = router;
